4759c1368d61fb671c023825fa26ecf0bb0d7166
Techpanda365 <techpanda354@gmail.com>
2026-03-13 17:37:26 +0530

    update time in node

--- backend/backend-node/Controllers/videoController.js
+++ backend/backend-node/Controllers/videoController.js
@@ -133,7 +133,7 @@ exports.uploadAndProcess = async (req, res) => {
         prompt: userPrompt,
       },
       {
-        timeout: 0,
+        timeout: 600000,
         maxContentLength: Infinity,
         maxBodyLength: Infinity,
       }
